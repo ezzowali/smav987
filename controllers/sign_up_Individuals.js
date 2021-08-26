3298ffc9588b0406fe5ecf7de2aa6dc98ea7bdb0
ezzowali <zwali.eng@gmail.com>
2021-08-26 18:19:28 +0300

    make it better15

--- controllers/sign_up_Individuals.js
+++ controllers/sign_up_Individuals.js
@@ -325,7 +325,7 @@ randomBytes(16, function(err, buffer)  {
           subject: 'اعادة كلمة المرور',
           html: `
             <p>You requested a password reset</p>
-            <p>Click this <a href="https://smav-hajj.smavacadmey ||"localhost:3000"/Individuals_new_password/${token}">link</a> to set a new password.</p>
+            <p>Click this <a href="https://smav-hajj.smavacadmey/Individuals_new_password/${token}">link</a> to set a new password.</p>
           `
         });
 
